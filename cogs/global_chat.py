2b0555221bf0841aad9ddc2b80d2968a30fb9fac
FirE-A <83416633+FirE-A@users.noreply.github.com>
2023-02-06 21:41:38 +0530

    Update global_chat.py

--- cogs/global_chat.py
+++ cogs/global_chat.py
@@ -15,7 +15,7 @@ class GlobalChat(commands.Cog):
         guild_id = ctx.message.guild.id
         channel_id = int(channel.strip('<#>'))
 
-        with open('.\\databases\\global_chat.json', 'r') as file:
+        with open('databases/global_chat.json', 'r') as file:
             global_chat_data = json.load(file)
             new_global_chat = str(guild_id)
 
@@ -26,7 +26,7 @@ class GlobalChat(commands.Cog):
             # Add new global chat
             else:
                 global_chat_data[new_global_chat] = channel_id
-                with open('.\\databases\\global_chat.json', 'w') as new_global_chat:
+                with open('databases/global_chat.json', 'w') as new_global_chat:
                     json.dump(global_chat_data, new_global_chat, indent=4)
 
                 await ctx.send(':white_check_mark: **Channel has been added to global chat!**')
@@ -36,13 +36,13 @@ class GlobalChat(commands.Cog):
     async def globalchatstop(self, ctx):
         guild_id = ctx.message.guild.id
 
-        with open('.\\databases\\global_chat.json', 'r') as file:
+        with open('databases/global_chat.json', 'r') as file:
             global_chat_data = json.load(file)
 
         global_chat_data.pop(str(guild_id))
 
         # Update global chat
-        with open('.\\databases\\global_chat.json', 'w') as update_global_chat_file:
+        with open('databases/global_chat.json', 'w') as update_global_chat_file:
             json.dump(global_chat_data, update_global_chat_file, indent=4)
 
         await ctx.send(':white_check_mark: **Channel has been removed from global chat!**')
@@ -51,7 +51,7 @@ class GlobalChat(commands.Cog):
     async def on_message(self, message):
         if not message.author.bot:
             if not message.content.startswith('!'):
-                with open('.\\databases\\global_chat.json', 'r') as file:
+                with open('databases/global_chat.json', 'r') as file:
                     global_chat_data = json.load(file)
 
                 channel_id = list(global_chat_data.values())
@@ -67,13 +67,18 @@ class GlobalChat(commands.Cog):
                     for ids in channel_id:
                         if message.channel.id != ids:
                             message_embed = nextcord.Embed(colour=randint(0, 0xffffff))
-
-                            message_embed.timestamp = datetime.datetime.utcnow()
+                            if message.author.avatar.url == None:
+                                icon_url="https://cdn.discordapp.com/avatars/1054719146304225285/e5ab0ff9744fc47e40b20a65aad406a8.png?size=1024"
+                            icon_url = message.author.avatar.url
+                            message_embed.timestamp = datetime.datetime.now()
                             message_embed.set_author(icon_url=message.author.avatar.url, name=f'{message.author}')
                             message_embed.description = f'**`Said: {message.content}`**'
-                            message_embed.set_footer(icon_url=message.guild.icon.url, text=message.guild.name)
-
-                            await self.bot.get_channel(ids).send(embed=message_embed)
+                            message_embed.set_footer(text=message.guild.name)
+                            try:
+                            	channel=await self.bot.fetch_channel(ids)
+                            	await channel.send(embed=message_embed)
+                            except Exception as e:
+                                print(e)
 
 
 def setup(bot):
